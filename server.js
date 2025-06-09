e234f25594de0fe6236e11a035eafa24de00dd24
HIMURA <zamtos79@gmail.com>
2025-06-09 12:39:30 +0300

    Navbar eklendi

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.static('public'));
 
 const Config = require('./config.json');
 
 const port = Config.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
