bd1ca0cd0c62cc9f6c5fc9e0d3c8cb9bfd3344ae
HIMURA <zamtos79@gmail.com>
2025-06-15 23:01:24 +0300

    giriş sistemi tamamlandı

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+})
+
 /*
 
 ROUTERS
