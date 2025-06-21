9330c61ca0650273d1fc8c8b412b80abecc2d086
HIMURA <zamtos79@gmail.com>
2025-06-21 12:49:40 +0300

    buglar düzeltildi

--- server.js
+++ server.js
@@ -131,6 +131,23 @@ client.on(Events.Error, error => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// CORS ve Cookie ayarları
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    
+    // SameSite cookie policy
+    res.header('Set-Cookie', 'SameSite=None; Secure');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // Statik dosyalar
 app.use(express.static(path.join(__dirname, 'public')));
 
