04d577502af55bf6b001669cde5b1b36d8c4ab2a
HIMURA <zamtos79@gmail.com>
2025-08-01 03:59:39 +0300

    bot tokeni eklendi

--- apps/bot/config.ts
+++ apps/bot/config.ts
@@ -1,6 +1,6 @@
 module.exports = {
     discord: {
-        token: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+        token: 'MTMxMDk1NDM1NTg4NDY5MTQ4OA.GKkUf9.TmLZ-8Gn5DbsLLtKGLuj-O4XWPB68JBW3OpyBw',
         clientId: '1310954355884691488'
     }
 }
