4f60218de93502fcf6c244c18ac368152db2556e
HIMURA <zamtos79@gmail.com>
2025-08-02 01:02:33 +0300

    navbar eklendi

--- apps/UI/src/components/navbar/navbar.tsx
+++ apps/UI/src/components/navbar/navbar.tsx
@@ -4,7 +4,14 @@ import './navbar.scss';
 const Navbar: React.FC = () => {
     return (
         <div className="navbar">
-
+            <div className="logo">PXDev.Com.tr</div>
+            <ul className="navLinks">
+                <li><a href="#Home">Anasayfa</a></li>
+                <li><a href="#about">Hakkımızda</a></li>
+                <li><a href="#products">Ürünler</a></li>
+                <li><a href="#services">Hizmetlerimiz</a></li>
+                <li><a href="#contact">İletişim</a></li>
+            </ul>
         </div>
     );
 };
