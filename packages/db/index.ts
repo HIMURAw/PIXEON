468103d651bdf71513e0b26363dcea6e4c31783f
HIMURA <zamtos79@gmail.com>
2025-08-03 02:38:52 +0300

    schema ayarları yapıldı

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,6 +1,7 @@
 // db/index.ts
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
+import { eq } from 'drizzle-orm';
 import * as schema from './schema/schema';
 
 const pool = mysql.createPool({
@@ -10,4 +11,5 @@ const pool = mysql.createPool({
     database: 'pxdevwebsitev3',
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema, mode: 'default' });
+export { schema, eq };
