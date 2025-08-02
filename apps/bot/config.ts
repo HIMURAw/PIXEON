9b8fd572e56d49638eaca510e486e304d79a6457
HIMURA <zamtos79@gmail.com>
2025-08-02 21:11:39 +0300

    bota yakında komutu eklendi

--- apps/bot/config.ts
+++ apps/bot/config.ts
@@ -1,6 +1,7 @@
 module.exports = {
     discord: {
         token: 'MTMxMDk1NDM1NTg4NDY5MTQ4OA.GKkUf9.TmLZ-8Gn5DbsLLtKGLuj-O4XWPB68JBW3OpyBw',
-        clientId: '1310954355884691488'
+        clientId: '1310954355884691488',
+        GuiId: '1269430268402335867'
     }
 }
