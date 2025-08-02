ff016fe0f19816b5b181f32f116b310e8ec9107b
HIMURA <zamtos79@gmail.com>
2025-08-03 02:47:03 +0300

    auth sistemi yapıldı

--- apps/bot/config.ts
+++ apps/bot/config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     discord: {
         token: 'MTMxMDk1NDM1NTg4NDY5MTQ4OA.GKkUf9.TmLZ-8Gn5DbsLLtKGLuj-O4XWPB68JBW3OpyBw',
         clientId: '1310954355884691488',
