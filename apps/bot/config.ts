8efe2431b2c24b331495c8a0e4e41376e72b5167
HIMURA <zamtos79@gmail.com>
2025-08-05 13:49:37 +0300

    bot configi ayarlandı

--- apps/bot/config.ts
+++ apps/bot/config.ts
@@ -1,9 +1,9 @@
 export default {
     discord: {
-        token: 'MTIzODI2ODU2MjE3MTIzNjM4NA.G9yZQu.t-I_wliv2EdcieoHbfGbugYV78ua2xz-DUTTmw',
+        token: 'MTIzODI2ODU2MjE3MTIzNjM4NA.GXx0Gu.NQeae0axbJC9_i7TyoIP_jkxnrSbpg9MDD2CWI',
         clientId: '1238268562171236384',
         GuiId: '1269430268402335867',
         redirectUri: 'http://localhost:3001/auth/discord/callback',
-        clientSecret: 'ix1HuTiiXWisx3LzbJVD2xgeHRRzK3Ca'
+        clientSecret: '5C0mlL1mOcC_IUQA0FkPehEevYS19jIY'
     }
 }
