7a958f1ef4cf7b7c82d06ddfee8bfa9e7fc338f1
HIMURA <zamtos79@gmail.com>
2025-08-12 02:32:45 +0300

    betterAuth eklendi ama database bağlantısı yapılmadığı için hata veriyor

--- apps/web/index.ts
+++ apps/web/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from "cookie-parser";
 import Config from './config';
-import authRoute from './routes/auth';
+import { auth } from './routes/auth';
 
 const app = express();
 const PORT = Config.port || 3001;
@@ -23,8 +23,25 @@ app.use((req, res, next) => {
 app.use(cookieParser());
 app.use(express.json());
 
-// ROUTERS 
-app.use('/auth', authRoute);
+
+app.all("/auth/(.*)", async (req, res) => {
+    const url = `http://localhost:${PORT}${req.originalUrl}`;
+    const request = new Request(url, {
+        method: req.method,
+        headers: req.headers as any,
+        body: req.method !== "GET" && req.method !== "HEAD" ? JSON.stringify(req.body) : undefined,
+    });
+
+    const response = await auth.handler(request);
+
+    response.headers.forEach((value, key) => {
+        res.setHeader(key, value);
+    });
+
+    res.status(response.status);
+    const text = await response.text();
+    res.send(text);
+});
 
 
 app.listen(PORT, () => {
--- apps/web/routes/auth.ts
+++ apps/web/routes/auth.ts
@@ -1,2 +1,12 @@
-import router from 'express';
-import { betterAuth } from 'better-auth'; 
+import { betterAuth } from "better-auth";
+import Config from "../config"
+
+export const auth = betterAuth({
+    socialProviders: {
+        discord: {
+            clientId: Config.discord.clientId as string,
+            clientSecret: Config.discord.clientSecret as string,
+            redirectUri: "http://localhost:3000/auth/callback",
+        },
+    },
+});
