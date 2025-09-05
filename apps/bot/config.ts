f882c76fd2eb78a97db63a75dc76ae7664bba8bb
HIMURA <zamtos79@gmail.com>
2025-09-05 15:56:33 +0300

    update

--- apps/bot/config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    discord: {
-        token: 'MTIzODI2ODU2MjE3MTIzNjM4NA.GXx0Gu.NQeae0axbJC9_i7TyoIP_jkxnrSbpg9MDD2CWI',
-        clientId: '1238268562171236384',
-        GuiId: '1269430268402335867',
-        redirectUri: 'http://localhost:3001/auth/discord/callback',
-        clientSecret: '5C0mlL1mOcC_IUQA0FkPehEevYS19jIY'
-    }
-}
--- sql.sql
+++
@@ -1,1 +0,0 @@
- CREATE DATABASE pxdevwebsitev3;
