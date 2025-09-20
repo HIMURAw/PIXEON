7dcd783761450950b26d6da0e69ce704e7645c09
HIMURA <zamtos79@gmail.com>
2025-09-20 04:21:05 +0300

    staffs.jsx dosyasının temeli atılıdı

---
+++ frontend/src/components/staffs/staffs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './staffs.scss';
+
+function Staffs() {
+    const staffs = [
+        {
+            name: "HIMURA",
+            role: "Founder & CEO",
+            image: "https://i.imgur.com/4YbX6bP.png",
+            social: {
+                twitter: "https://twitter.com/HimuraDev",
+                github: "github.com/HIMURAw",
+            }
+        },
+    ]
+    return (
+        <div className="staffs-container">
+
+        </div>
+    )
+}
+
+export default Staffs;
