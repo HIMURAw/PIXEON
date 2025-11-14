fdf1da7e12cddc968f039b1b6ff8a2999390035d
HIMURA <zamtos79@gmail.com>
2025-11-14 23:57:52 +0300

    added profile section

--- src/components/CountryDropDown/index.js
+++ src/components/CountryDropDown/index.js
@@ -7,7 +7,7 @@ const countryDrop=() => {
             <Button className='countryDrop'>
                 <div className='info d-flex flex-column'>
                     <span className='label'>Your Location</span>
-                    <span className='name'>India</span>
+                    <span className='name'>Turkey</span>
                 </div>
                 <span className='ml-auto'><FaAngleDown /></span>
             </Button>
