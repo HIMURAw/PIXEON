7e33d6da794622b19eb4b27b4e9c488046026aee
HIMURA <zamtos79@gmail.com>
2025-11-14 23:30:00 +0300

    Update

--- src/components/CountryDropDown/index.js
+++ src/components/CountryDropDown/index.js
@@ -6,7 +6,7 @@ const countryDrop=() => {
         <>
             <Button className='countryDrop'>
                 <div className='info d-flex flex-column'>
-                    <span className='lable'>Your Location</span>
+                    <span className='label'>Your Location</span>
                     <span className='name'>India</span>
                 </div>
                 <span className='ml-auto'><FaAngleDown /></span>
