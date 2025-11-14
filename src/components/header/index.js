3bcbe5f02e184faa4496c2ec1e8983dae3aedb93
HIMURA <zamtos79@gmail.com>
2025-11-14 22:32:14 +0300

    Update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,15 @@
 const Header = () => {
     return (
         <>
-            
+            <div className="headerWrapper">
+                <div className="top-strip bg-blue">
+                    <div className="container">
+                        <p className="mb-0 mt-0 text-center">Sitemizde <b>"HOSGELDIN12"</b> kodu ile %10 kar edebilirsiniz</p>
+                    </div>
+                </div>
+
+                <div></div>
+            </div>
         </>
     );
 }
