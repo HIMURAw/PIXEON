d9883b21de5d16cc18288ab49db11258069df589
HIMURA <zamtos79@gmail.com>
2025-11-16 21:18:51 +0300

    added country modal

--- src/components/CountryDropDown/index.js
+++ src/components/CountryDropDown/index.js
@@ -1,6 +1,9 @@
 import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
 import { FaAngleDown } from 'react-icons/fa6';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import { IoIosSearch } from "react-icons/io";
+
 
 const countryDrop=() => {
     return (
@@ -13,6 +16,16 @@ const countryDrop=() => {
                 <span className='ml-auto'><FaAngleDown /></span>
             </Button>
 
+
+            <Dialog open={true} className='locationModal'>
+                <h4>Choose your Delivery Location</h4>
+                <p>Enter your adress and we will specify thr offer your area.</p>
+
+                <div className='headerSearch ml-3 mr-3 '>
+                    <input type='text' placeholder='Search for products...' />
+                    <Button><IoIosSearch /></Button>
+                </div>
+            </Dialog>
         </>
     );
 }
