8de7a42eed6d4f5434f6a82ee2b6ea1885b292b7
HIMURA <zamtos79@gmail.com>
2025-12-28 23:35:54 +0300

    Refactor code structure for improved readability and maintainability

---
+++ frontend/components/header/Head.tsx
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import Image from "next/image";
+import {
+    Hand,
+    ChevronDown,
+    Search,
+    ShoppingCart,
+    User
+} from "lucide-react";
+
+export default function Head() {
+    return (
+        <header className="w-full border-b border-gray-200">
+            <div className="max-w-7xl mx-auto px-6">
+                <div className="flex items-center justify-between py-4">
+                    <div className="flex flex-col items-center gap-1">
+                        <Image
+                            className="object-contain"
+                            src="/logo.png"
+                            alt="Logo"
+                            width={96}
+                            height={96}
+                        />
+                        <span className="text-xs text-gray-500 text-center">
+                            Online Grocery Shopping Center
+                        </span>
+                    </div>
+
+                    <button className="hidden h-15 md:flex items-center border border-gray-600 rounded-lg pl-6 pr-4 py-2 gap-4 ml-9">
+                        {/* TEXT */}
+                        <div className="flex flex-col items-start leading-tight">
+                            <span className="text-gray-400 text-xs">Your Location</span>
+                            <span className="font-medium text-[#7dd7fb]">Select a Location</span>
+                        </div>
+
+                        {/* ICON */}
+                        <ChevronDown size={16} className="ml-auto" />
+                    </button>
+
+                    <div className="flex-1 mx-6 hidden md:block ml-10">
+                        <div className="relative">
+                            <input
+                                type="text"
+                                placeholder="Search for products..."
+                                className="w-full h-15 bg-[#f3f4f7] text-black rounded-lg px-4 py-3 pr-10 outline-none"
+                            />
+                            <Search
+                                size={18}
+                                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex items-center gap-4">
+                        <div className="flex items-center gap-2">
+                            <div className="bg-white w-10 h-10 flex items-center justify-center border border-gray-600 rounded-full">
+                                <User color="black" size={16} />
+                            </div>
+                            <span className="font-medium">$0.00</span>
+                        </div>
+
+
+                        <div className="flex items-center gap-2">
+                            <div className="w-10 h-10 bg-orange-100 flex items-center justify-center border border-gray-600 rounded-full">
+                                <ShoppingCart color="black" size={16} />
+                            </div>
+                            <span className="font-medium">0</span>
+                        </div>
+
+                    </div>
+                </div>
+
+                <nav className="flex items-center gap-6 py-3 text-sm font-medium">
+                    <button className="flex items-center gap-2 bg-sky-400 text-white px-5 py-3 rounded-full">
+                        ALL CATEGORIES
+                        <ChevronDown size={16} />
+                    </button>
+
+                    <Link href="#">HOME</Link>
+                    <Link href="#" className="flex items-center gap-1">
+                        SHOP <ChevronDown size={14} />
+                    </Link>
+                    <Link href="#">MEATS & SEAFOOD</Link>
+                    <Link href="#">BAKERY</Link>
+                    <Link href="#">BEVERAGES</Link>
+                    <Link href="#">BLOG</Link>
+                    <Link href="#">CONTACT</Link>
+                </nav>
+            </div>
+        </header>
+    );
+}
--- frontend/components/header/MainBar.tsx
+++ frontend/components/header/MainBar.tsx
@@ -1,31 +1,35 @@
+import {
+    Hand
+} from "lucide-react";
+
 export default function MainBar() {
     return (
-        <div className="bg-black text-gray-300 text-sm">
+        <div className="bg-black text-gray-300 text-sm border-b border-gray-600">
             <div className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3">
 
                 <div className="flex gap-4">
-                    <a href="#" className="hover:text-white">About Us</a>
-                    <a href="#" className="hover:text-white">My Account</a>
-                    <a href="#" className="hover:text-white">Wishlist</a>
-                    <a href="#" className="hover:text-white">Order Tracking</a>
+                    <a href="#" className="hover:text-[#7dd7fb] text-[#696e7f]">About Us</a>
+                    <a href="#" className="hover:text-[#7dd7fb] text-[#696e7f]">My Account</a>
+                    <a href="#" className="hover:text-[#7dd7fb] text-[#696e7f]">Wishlist</a>
+                    <a href="#" className="hover:text-[#7dd7fb] text-[#696e7f]">Order Tracking</a>
                 </div>
 
-                <div className="hidden md:flex items-center gap-2 text-gray-400">
-                    <span>🔒</span>
+                <div className="hidden md:flex items-center gap-2 text-gray-400 border-r border-gray-600 pr-4">
+                    <Hand />
                     <span>Kuryeyle iletişime geçmeden %100 güvenli teslimat.</span>
                 </div>
 
                 <div className="flex items-center gap-4">
-                    <span className="text-cyan-400 font-semibold">
-                        <a className="" href="#">Need help?</a> Call Us: +0020 500
+                    <span className="text-[#696e7f] font-semibold border-r border-gray-600 pr-4">
+                        <a className="hover:text-[#7dd7fb]" href="#">Need help?</a> Call Us: <span className="text-cyan-400"> +90 552 833 08 83</span>
                     </span>
 
-                    <select className="bg-black border border-gray-600 text-white text-sm px-2 py-1">
+                    <select className="bg-black border-none font-bold text-white text-sm px-2 py-1">
                         <option>English</option>
                         <option>Türkçe</option>
                     </select>
 
-                    <select className="bg-black border border-gray-600 text-white text-sm px-2 py-1">
+                    <select className="bg-black  border border-none font-bold text-white text-sm px-2 py-1">
                         <option>USD</option>
                         <option>TRY</option>
                     </select>
