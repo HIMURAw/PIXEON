14dfecf4d926fdabb55676b0bb2c203cc7543642
HIMURA <zamtos79@gmail.com>
2025-12-30 22:45:24 +0300

    Refactor code structure for improved readability and maintainability

--- frontend/app/(public)/page.tsx
+++ frontend/app/(public)/page.tsx
@@ -1,7 +1,7 @@
 import MainBar from "@/components/header/MainBar";
 import TopBar from "@/components/header/TopBar";
 import Head from "@/components/header/Head";
-
+import HeroCarousel from "@/components/hero/hero";
 
 export default function Home() {
     return (
@@ -9,6 +9,18 @@ export default function Home() {
             <TopBar />
             <MainBar />
             <Head />
+            <div className="container mx-auto px-4 py-8">
+                <div className="flex gap-6">
+                    {/* Sidebar kategoriler */}
+                    <aside className="w-72">
+                        {/* Kategorileriniz buraya */}
+                    </aside>
+
+                    <main className="flex-1">
+                        <HeroCarousel />
+                    </main>
+                </div>
+            </div>
         </>
     );
 }
