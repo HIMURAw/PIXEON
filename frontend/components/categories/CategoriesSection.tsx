77410a84103cd4f0fcce6abd0b6b5c673e1c62e8
HIMURA <zamtos79@gmail.com>
2026-01-02 16:27:29 +0300

    Update

--- frontend/components/categories/CategoriesSection.tsx
+++ frontend/components/categories/CategoriesSection.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import CategoriesButton from "../header/categoriesButton/categoriesButton";
 import CategoriesMenu from "./CategoriesMenu";
+import PromoSection from "../hero/promoSection";
 
 export default function CategoriesSection() {
     const [open, setOpen] = useState(true);
