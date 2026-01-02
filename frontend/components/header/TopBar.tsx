36ab22b18070d5aa807a4a6e164c2ce639b26a75
HIMURA <zamtos79@gmail.com>
2026-01-02 16:07:57 +0300

    Küçük renk düzeltmeleri yapıldı

--- frontend/components/header/TopBar.tsx
+++ frontend/components/header/TopBar.tsx
@@ -2,11 +2,11 @@ export default function TopBar() {
     return (
         <>
             <div className="hidden md:block bg-sky-500 text-white text-sm text-center py-2 font-semibold">
-                Sitemize özel indirimlerle alışveriş yapın! Tüm siparişlerde ücretsiz kargo fırsatını kaçırmayın.
+                Shop with exclusive discounts on our website! Free shipping on all orders.
             </div>
 
             <div className="md:hidden bg-sky-500 text-white text-xs text-center py-2 font-semibold px-3">
-                Ücretsiz kargo fırsatı! Kaçırma.
+                Free shipping! Don't miss out!
             </div>
         </>
     );
